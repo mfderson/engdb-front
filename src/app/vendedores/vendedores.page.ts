194964e77768bba3ee1f911a8086aa845773e666
Anderson <mfderson@gmail.com>
2019-03-05 13:58:36 -0300

    Mostrando vendedores na lista - Parte 1

--- src/app/vendedores/vendedores.page.ts
+++ src/app/vendedores/vendedores.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VendedorService } from 'src/services/domain/vendedor.service';
+import { VendedorViewDTO } from 'src/models/vendedor.view.dto';
 
 @Component({
   selector: 'app-vendedores',
@@ -8,12 +9,26 @@ import { VendedorService } from 'src/services/domain/vendedor.service';
 })
 export class VendedoresPage implements OnInit {
 
+  items: VendedorViewDTO[];
+
   constructor(public vendedorService: VendedorService) { }
 
   ngOnInit() {
+    // this.items = [
+    //   {
+    //     id: "1",
+    //     nome: "Bosta do Peruca",
+    //     cpf: "12345678912"
+    //   },
+    //   {
+    //     id: "2",
+    //     nome: "Bosta do Nandim",
+    //     cpf: "12345678913"
+    //   }
+    // ]
     this.vendedorService.findAll()
       .subscribe(response => {
-        console.log(response);
+        this.items = response['content'];
       },
       error => {
         console.log(error);
